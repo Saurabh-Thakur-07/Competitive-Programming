3b8fb1802b8278755568430bda6bc044d77b0741
Saurabh-Thakur-07 <77320702+Saurabh-Thakur-07@users.noreply.github.com>
2022-11-20 22:19:58 +0530

    Create q1.java

---
+++ LeetCode/Weekly-320/q1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                for(int k=j+1;k<n;k++)
+                {
+                    if((nums[i] != nums[j])&&(nums[i] != nums[k])&&(nums[j] != nums[k]))count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+}
