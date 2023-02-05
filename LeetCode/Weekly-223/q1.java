9b7576cf5df3459677a268461c993455f462cf7d
Saurabh-Thakur-07 <77320702+Saurabh-Thakur-07@users.noreply.github.com>
2023-02-05 13:11:09 +0530

    Create q1.java

---
+++ LeetCode/Weekly-223/q1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int n=encoded.length+1;
+        int[] ans = new int[encoded.length+1];
+        ans[0]=first;
+        for(int i=1;i<n;i++)
+        {
+            ans[i] = ans[i-1]^encoded[i-1];
+        }
+        return ans;
+    }
+}
